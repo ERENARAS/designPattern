c9039906f74d05de4ddcd8766c9036f253e67f09
ERENARAS <116448375+ERENARAS@users.noreply.github.com>
2022-10-25 17:05:11 +0300

    Add files via upload

---
+++ Singleton/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var customerManager = CustomerManager.CreateAsSingleton();
+
+            customerManager.Save();
+            Console.ReadLine();
+        }
+    }
+    class CustomerManager
+    {
+
+        private static CustomerManager _customerManager;
+
+        private CustomerManager()
+        {
+        }
+
+        public static CustomerManager CreateAsSingleton()
+        {
+            return _customerManager ?? (_customerManager = new CustomerManager());
+        }
+
+        public void Save()
+        {
+            Console.WriteLine("Saved!!");
+        }
+
+
+    }
+}
